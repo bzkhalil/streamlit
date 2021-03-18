64a0cc5a9b4f399896ea76842a3afc7c978364a7
bzkhalil <bzkhalil@users.noreply.github.com>
2021-03-19 00:39:42 +0100

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
+from pytube import YouTube
+
 import numpy as np
 import pandas as pd
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.title("Youtube Video Donwloader")
+st.subheader("Enter the URL:")
+url = st.text_input(label='URL')
